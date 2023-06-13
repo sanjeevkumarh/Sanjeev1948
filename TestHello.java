f7abd50df2b9825fa70de9f31e54555f54ea37e9
sanjeevkumarh <53606171+sanjeevkumarh@users.noreply.github.com>
2023-06-13 15:56:47 +0530

    Update TestHello.java

--- TestHello.java
+++ TestHello.java
@@ -1,4 +1,4 @@
-public class Hello{
+public class TestHello{
                      public static void main(String[] args){
                             System.out.println("Hello World New Jenkins Session!!");
                             }
