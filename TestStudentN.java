da5a524e76a577bb9fa8c4a9619e5a81de3ba58a
sanjeevkumarh <53606171+sanjeevkumarh@users.noreply.github.com>
2023-06-13 17:08:54 +0530

    Update TestStudentN.java

--- TestStudentN.java
+++ TestStudentN.java
@@ -1,1 +1,6 @@
-
+public class TestStudentN{
+                     public static void main(String[] args){
+                            System.out.println("School  Having 25 students in 10th Standard!!");
+                            System.out.println("Ankit received 1st Positions!!");
+                            }
+              } 
